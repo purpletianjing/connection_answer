29f07489aa4f14d6414b02a795ff94aa24df0aef
sialvsic <sialvsic@outlook.com>
2016-03-23 16:28:26 +0800

    set answer

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,6 +2,12 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var collection_result = [];
+  collection_result = collection.filter(function(ele){
+    return (ele % 2 == 0);
+  });
+  return collection_result;
+
 }
 
 module.exports = collect_all_even;
